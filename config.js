4b083356ce4e752f362c63a5489675dbf21a1f13
h2106239-spec <h2106239@gmail.com>
2026-08-08 21:32:49 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-window.FAMILY_ASSET_CONFIG={API_URL:"",REFRESH_MS:0};
+window.FAMILY_ASSET_CONFIG={
+  API_URL:"https://script.google.com/macros/s/AKfycbyHGn9vuqY7r-0r5Sx_keHjkMi543801uw5TCzNY0LzjJqWyhALFrA9PVv5n9xMl0dx0w/exec",
+  REFRESH_MS:60000
+};
